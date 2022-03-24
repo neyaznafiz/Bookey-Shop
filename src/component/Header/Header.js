9c0f7308bbed04c69f57f14fa360a44c7672763c
niiyazobug <niiyaz5555@gmail.com>
2022-03-24 17:36:36 +0600

    navbar added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            
+        <nav className='nav'>
+            <h3>BOOKEY SHOP</h3>
+        <div className=''>
+            <a href="/home">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <a href="/shopnow">Shop Now</a>
         </div>
+        </nav>
     );
 };
 
