745864ec6a37459e5b79bb240c32487d5c888609
niiyazobug <niiyaz5555@gmail.com>
2022-03-25 02:06:31 +0600

    total price added in cart

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -5,12 +5,24 @@ const Cart = ({ cart }) => {
 
     // const {cart} = props
 
+    let total = 0
+    // let delivery = 0
+    // let quantity = 0
+
+    for (const flower of cart) {
+        console.log(flower);
+        total = total + flower.price
+        // quantity = quantity + flower.quantity
+        // total = total + flower.price * flower.quantity
+    }
+
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             {/* cart ditails show */}
             <p>Selected Items : {cart.length}</p>
-            <p>Total Price : </p>
+            <p>Quantity : { }</p>
+            <p>Total Price : ${total}</p>
             <p>Delivery Charge : </p>
             <p>Tax :</p>
             <h5>Grand Total :</h5>
